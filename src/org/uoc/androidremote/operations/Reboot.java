c0a5de29f0f1a41091e01f06a36431ad1867b801
Robert Ramírez Vique <robertrv@gmail.com>
2011-11-28 20:56:13 +0100

    changing reboot operation

--- src/org/uoc/androidremote/operations/Reboot.java
+++ src/org/uoc/androidremote/operations/Reboot.java
@@ -2,7 +2,7 @@ package org.uoc.androidremote.operations;
 
 import java.io.Serializable;
 
-public class OperationReboot implements Serializable {
+public class Reboot implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private boolean result;
