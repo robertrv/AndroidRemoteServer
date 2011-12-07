0821f0900beb6f66962e56752f15f0f7cdfa25c7
Robert Ramírez Vique <robertrv@gmail.com>
2011-12-07 22:01:47 +0100

    fixing unnecessary import

--- src/org/uoc/androidremote/server/Utils.java
+++ src/org/uoc/androidremote/server/Utils.java
@@ -3,15 +3,14 @@ package org.uoc.androidremote.server;
 import java.io.File;
 
 import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.ActivityManager.RunningServiceInfo;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 /**
  * Utility methods mainly to manage starting and ending of our servers
