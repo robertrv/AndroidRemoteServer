37a12f867072f988bfc7686957af2f5b647a4e23
Robert Ramírez Vique <robertrv@gmail.com>
2012-03-11 19:35:41 +0100

    added small things to get install apk working through network

--- prepare.sh
+++ prepare.sh
@@ -6,16 +6,34 @@ die () {
 
 [ "$#" -eq 1 ] || die "1 argument required, $# provided"
 
-emulator -avd $1 -partition-size 128 &
-adb -e wait-for-device
-adb -e remount
-adb -e push ./res/raw/su /system/bin
-adb -e shell chmod 6755 /system/bin/su
-adb -e shell rm /system/xbin/su
-adb -e shell ln -s /system/bin/su /system/xbin/su
-adb -e push ./res/raw/busybox /system/bin
-adb -e shell chmod 6755 /system/bin/busybox
-adb -e install ./others/SuperUser.apk
-adb forward tcp:5901 tcp:5901
-adb forward tcp:5000 tcp:5000
+port=5554
+isBusy=`adb devices|grep $port|wc -l`
+while [ $isBusy -gt 0 ]
+do
+    port=$(( $port + 2 ))
+    isBusy=`adb devices|grep $port|wc -l`
+done
+
+echo "free port: $port"
+emulator -port $port -avd $1 -partition-size 128 &
+# Has to sleep to get proper devices return value
+serial=`adb devices |grep $port|cut -f1`
+while [ -z $serial ] 
+do
+    sleep 1
+    serial=`adb devices |grep $port|cut -f1`
+done
+echo "serial: $serial"
+
+adb -s $serial -e wait-for-device
+adb -s $serial -e remount
+adb -s $serial -e push ./res/raw/su /system/bin
+adb -s $serial -e shell chmod 6755 /system/bin/su
+adb -s $serial -e shell rm /system/xbin/su
+adb -s $serial -e shell ln -s /system/bin/su /system/xbin/su
+adb -s $serial -e push ./res/raw/busybox /system/bin
+adb -s $serial -e shell chmod 6755 /system/bin/busybox
+adb -s $serial -e install ./others/SuperUser.apk
+adb -s $serial forward tcp:5901 tcp:5901
+adb -s $serial forward tcp:5000 tcp:5000
 
--- src/org/uoc/androidremote/operations/Operation.java
+++ src/org/uoc/androidremote/operations/Operation.java
@@ -20,8 +20,6 @@ package org.uoc.androidremote.operations;
 import java.io.Serializable;
 
 /**
- * The Class Operation.
- * 
  * @author angel
  */
 public class Operation implements Serializable {
@@ -34,6 +32,7 @@ public class Operation implements Serializable {
     public static final int OP_APPLICATIONS_INSTALLED = 5;
     public static final int OP_BATTERY_LEVEL = 6;
     public static final int OP_REBOOT = 7;
+    public static final int OP_INSTALL_APPLICATION = 8;
     private static final long serialVersionUID = 1L;
     
     /** The id. */
